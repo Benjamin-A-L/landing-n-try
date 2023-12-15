a78900d4fa2a45937eb8ff2285c97632c9120117
benja <benjamin.astudillo.loyer@gmail.com>
2023-12-15 07:21:00 -0300

    feat small changes for optimization

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-image',
+    'gatsby-plugin-smoothscroll',
+    'gatsby-plugin-react-helmet',
+  ],
+}
