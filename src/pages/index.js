302ef4275166475afb3735fcdc966d4c42b36a4d
benja <benjamin.astudillo.loyer@gmail.com>
2024-01-13 10:29:48 -0300

    fix share link image attempt #7

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,8 +57,8 @@ const IndexPage = () => {
 
       <div class="mainWrapper">
         <Helmet>
-          {/* <meta name="description" content="Diseño web para bares y restaurantes" /> */}
-          {/* <meta property="og:image" content="C:\Users\Benchmon\Desktop\productividad\HoldTML\landing page-v1 netlify deploy copy\landing-v1-netlify\static\HoldTML-logo.jpg" /> */}
+          <meta name="description" content="Diseño web para bares y restaurantes" />
+          <meta name="og:image" content="C:\Users\Benchmon\Desktop\productividad\HoldTML\landing page-v1 netlify deploy copy\landing-v1-netlify\static\HoldTML-logo.jpg" />
         </Helmet>
         <section className="headline">
           <Logo className="logo-head"></Logo>
@@ -169,12 +169,13 @@ const IndexPage = () => {
 
 export default IndexPage
 
-export const Head = () => {
-  return(
-    <head>
-      <title>HoldTML</title>
-      <meta name='description' content='Diseño web para bares y restaurantes' />
-      <meta name='og:image' content='../static/HoldTML.jpg' />
-    </head>
-  )
-}
+export const Head = () => <title>HoldTML</title>
+// export const Head = () => {
+//   return(
+//     <head>
+//       <title>HoldTML</title>
+//       <meta name='description' content='Diseño web para bares y restaurantes' />
+//       <meta name='og:image' content='../static/HoldTML.jpg' />
+//     </head>
+//   )
+// }
